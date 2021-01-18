e6708eaf48236338184aa90c97cd46c4c4c3e9fe
Rafael Rodriguez <rrs@brown.edu>
2021-01-18 14:34:22 -0500

    created partial function specification, refactor domain and codomain classes

--- lmdp/grounding/GroundingClass.py
+++ lmdp/grounding/GroundingClass.py
@@ -3,37 +3,7 @@
     Author: Rafael Rodriguez-Sanchez (rrs@brown.edu)
     Date: August 2020
 """
-
-class Domain:
-    DOMAINS = {0: "State", 1: "Action", 2: "Next State"}
-    INV_DOMAINS = dict(zip(DOMAINS.values(), DOMAINS.keys()))
-    def __init__(self, domain):
-        self._domain = set([Domain.INV_DOMAINS[d] for d in domain])
-
-    def __str__(self):
-        if (len(self._domain) == 0):
-            return "[]"
-        else:
-            d = [Domain.DOMAINS[d] for d in sorted(list(self._domain))]
-            return ",".join(d)
-
-    def __call__(self):
-        return [Domain.DOMAINS[d] for d in self._domain]
-
-    def is_sa(self):
-        return 0 in self._domain and 1 in self._domain and 2 not in self._domain
-
-    def is_sas(self):
-        return 0 in self._domain and 1 in self._domain and 2 in self._domain
-
-    def is_s(self):
-        return 0 in self._domain and 1 not in self._domain and 2 not in self._domain
-
-    def is_a(self):
-        return 0 not in self._domain and 1 in self._domain and 2 not in self._domain
-
-    def is_ss(self):
-        return 0 in self._domain and 1 not in self._domain and 2 in self._domain
+from lmdp.utils.expression_utils import Domain, Codomain
 
 class Grounding(object):
     def __init__(self, name=None, domain=[]):
---
+++ lmdp/grounding/PartialFunctionClass.py
@@ -0,0 +1,31 @@
+'''
+    Partial Function Specification Class
+        - Represents function specification, with a given domain-codomain, 
+        in the form of a function by parts. 
+        - Implemented as Pairs of (boolean expressions, function).
+    author: Rafael Rodriguez-Sanchez (rrs@brown.edu)
+    date: January 2021
+'''
+from lmdp.utils.expression_utils import Domain, Codomain
+
+class PartialFunction:
+
+    def __init__(self, domain=[], codomain=[]):
+        self.specification = [] # list of (boolean, function) specification
+        self.domain = Domain(domain)
+        self.codomain = Codomain(codomain)
+
+    def __call__(self, *args):
+        r = []
+        for spec in self.specification:
+            if spec[0](*args):
+                r.append(spec[2](*args))
+        return r
+
+    def add_specification(self, boolean_cond, function):
+        if (self.domain == boolean_cond.domain and self.codomain == function.codomain and self.domain == function.domain):
+            self.specification.append((boolean_cond, function))
+        elif (self.domain == boolean_cond.domain):
+            raise "Domains are not compatible: " + str(self.domain) + "~=" + str(boolean_cond.domain)
+        else:
+            raise "Codomains are not compatible: " + str(self.codomain) + "~=" + str(boolean_cond.codomain) " or " + str(self.codomain) + "~=" + str(function.codomain)
