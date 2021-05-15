8e3fd49a604221f978071866bee44f3cbb05cee7
Rafael Rodriguez <rrs@brown.edu>
2021-05-14 21:03:49 -0400

    fix cli args bug

--- examples/craftworld/craftworld_dqn.py
+++ examples/craftworld/craftworld_dqn.py
@@ -10,32 +10,42 @@ import lmdp.agents.dqn as dqn
 
 import argparse
 
+def _get_args(args, dict_args):
+    r = {}
+    for arg in args:
+        if arg in dict_args:
+            r[arg] = dict_args[arg]
+    return r
+
+
 parser = argparse.ArgumentParser(description='Craftworld-RLang Arguments', add_help=False)
 parser.add_argument('--device', type=str, default='cpu', help='Device to run: cpu or gpu')
 
 # Experiment params
-exp_parser = argparse.ArgumentParser(parents=[parser], description='Craftworld-RLang Experiment Arguments')
-exp_parser.add_argument('--nruns', type=int, default=1, help='Number of Runs')
-exp_parser.add_argument('--frames', type=int, default=1000, help='Max number of timesteps')
-exp_parser.add_argument('--test_episodes', type=int, default=100, help='Episodes to evaluate Agent')
-exp_parser.add_argument('--seed', type=int, default=argparse.SUPPRESS, help='Random Generator seed')
-exp_parser.add_argument('--logdir', type=str, default='results/craftworld/runs', help='Result logging directory')
-exp_parser.add_argument('--name', type=str, default='craft-dqn')
+experiment_params = ['nruns', 'frames', 'test_episodes', 'seed', 'logdir', 'name', 'device']
+
+parser.add_argument('--nruns', type=int, default=1, help='Number of Runs')
+parser.add_argument('--frames', type=int, default=1000, help='Max number of timesteps')
+parser.add_argument('--test_episodes', type=int, default=100, help='Episodes to evaluate Agent')
+parser.add_argument('--seed', type=int, default=argparse.SUPPRESS, help='Random Generator seed')
+parser.add_argument('--logdir', type=str, default='results/craftworld/runs', help='Result logging directory')
+parser.add_argument('--name', type=str, default='craft-dqn')
 
 # Agent params
-agent_parser = argparse.ArgumentParser(parents=[parser], description='Craftworld-RLang Agent Arguments')
-agent_parser.add_argument('--state_feature_dim', type=int, default=256, help='Final feature dimension')
+parser.add_argument('--state_feature_dim', type=int, default=256, help='Final feature dimension')
+agent_params = list(dqn.default_cli_args.keys()) + ['state_feature_dim']
 for k, v in dqn.default_cli_args.items():
-    agent_parser.add_argument(f"--{k}", type=type(v), default=v)
+    parser.add_argument(f"--{k}", type=type(v), default=v)
 
 # Environment params
-env_parser = argparse.ArgumentParser(parents=[parser], description='Craftworld-RLang Environment Arguments')
-env_parser.add_argument('--goal', type=str, default='gold', help='Craftworld Goal')
+environment_params = ['goal']
+parser.add_argument('--goal', type=str, default='gold', help='Craftworld Goal')
 
 # params dicts
-exp_params = vars(exp_parser.parse_args())
-agent_params = vars(agent_parser.parse_args())
-env_params = vars(env_parser.parse_args())
+params = vars(parser.parse_args())
+exp_params = _get_args(experiment_params, params)
+agent_params = _get_args(agent_params, params)
+env_params = _get_args(environment_params, params)
 
 # Environment Setup
 craft = Craftworld(**env_params)
