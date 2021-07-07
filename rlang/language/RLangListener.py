3153cec065831b7880fcd34640738ed5614ad8e1
SkittlePox <bspiegel11@gmail.com>
2021-07-07 13:03:05 -0400

    Added empty listener file

---
+++ rlang/language/RLangListener.py
@@ -0,0 +1,13 @@
+from antlr4 import *
+from RLangLexer import RLangLexer
+from RLangParser import RLangParser
+from RLangParserListener import RLangParserListener
+import sys
+
+
+def main():
+    print("imports working")
+
+
+if __name__ == '__main__':
+    main()
