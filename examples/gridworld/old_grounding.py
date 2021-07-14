717f07cfdc08303f277fad04e78a43c98bc70ccb
jennjwang <jennifer123wang@gmail.com>
2021-07-14 10:59:53 -0400

    add new examples folder

---
+++ examples/gridworld/old_grounding.py
@@ -0,0 +1,52 @@
+import sys, os
+sys.path.append(os.path.abspath("./"))
+from simple_rl.tasks.grid_world.GridWorldMDPClass import GridWorldMDP
+from lmdp.grounding import *
+from lmdp.grounding.states.StateClass import State, BatchedState
+from lmdp import *
+
+# MDP parameters
+width, height = 6,6
+lava_locs=[(3,2), (1,4), (2,4), (2,5)]
+walls=[(3, 1)]
+goal_locs=[(5,1)]
+
+#### Factors
+position = StateFactor([0,1], "position") # definition of new factor.
+x, y = position[0], position[1]
+
+
+### Symbols
+goal = Symbol(bool_and(x == 5, y == 1))
+wall = Symbol(bool_and(x == 3, y == 1))
+lava = Symbol(position.in_(lava_locs))
+
+### Effects
+@effect
+def effect_action(state, action): # actions effects on state/Transition Dynamics
+    if action == 'up':
+        return position(state) + (0,1)
+    if action == 'down':
+        return position(state) + (0,-1)
+    if action == 'left':
+        return position(state) + (-1, 0)
+    if action == 'right':
+        return position(state) + (1, 0)
+
+effect_action = PredictiveEffect(bool_and(any_state, any_action), action_effect)
+
+mdp = GridWorldMDP(width, height, walls=walls, lava_locs=lava_locs, goal_locs=goal_locs, slip_prob=0.33, step_cost=0)
+
+lmdp = LMDP(mdp, factor_names=["x", "y"])
+
+ # how to convert this to RLang and was there a reason why it wasn't in the grounding/vocab file? 
+ # Is there a way to separate lmdp from the following effects if we make when into a keyword for a conditional / is it possible to express them as effects? 
+ 
+with lmdp.when(lava @ effect_action) as c: # when you fall in lava (@ is the function composition operator)
+    c.reward(-1)
+with c.otherwise().when(goal @ effect_action): # otherwise when in goal
+    c.reward(1)
+with lmdp.when(wall @ effect_action) as c: # when the action taken takes you to a wall position 
+    c.effect(S) # next state is the current state
+with lmdp.when(bool_and(any_state, any_action)) as c:
+    c.effect(effect_action)
---
+++ examples/gridworld/rmax_gridworld.py
@@ -0,0 +1,51 @@
+import sys, os
+sys.path.append(os.path.abspath("./"))
+# envs
+from simple_rl.tasks.grid_world.GridWorldMDPClass import GridWorldMDP
+from simple_rl.tasks.grid_world.GridWorldStateClass import GridWorldState
+#agents
+from lmdp.agents import RMaxLangAgent, RMaxAgent, RandomAgent
+#lmdp
+from lmdp import *
+from lmdp.experiment_runner import *
+from lmdp.experiment_runner import run_agents
+from functools import partial
+
+import numpy as np
+
+def experiment_params():
+    return {"instances":3, 
+            "episodes": 300, 
+            "steps":100,
+            "clear_old_results":True,
+            "rew_step_count":1,
+            "track_disc_reward":False,
+            "open_plot":True,
+            "verbose":False,
+            "reset_at_terminal":False,
+            "cumulative_plot": True,
+            "dir_for_plot":"results/rmax",
+            "experiment_name_prefix":"",
+            "track_success":False,
+            "success_reward":None}
+
+
+def gridworld_state_space(width, height): # state space iterator
+    for x in range(1,width+1):
+        for y in range(1, height+1):
+            yield GridWorldState(x, y)
+
+if __name__ == "__main__":
+    from grounding import *
+
+    #### Run agents
+    
+    random = RandomAgent(mdp.get_actions())
+    lang_rmax_agent = RMaxLangAgent(mdp.get_actions(), lmdp=lmdp,  s_a_threshold=30)
+    lang_rmax_agent.update_from_lang(partial(gridworld_state_space, width, height))
+    rmax_agent = RMaxAgent(mdp.get_actions(), s_a_threshold=30, max_reward=1)
+
+    run_agents([lang_rmax_agent, rmax_agent], mdp, experiment_params())
+
+    
+
