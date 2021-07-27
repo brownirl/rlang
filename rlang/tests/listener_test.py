a43f596819b69a1a9522fa79d38607c08af482bb
SkittlePox <bspiegel11@gmail.com>
2021-07-26 21:07:56 -0400

    Added listener test for Factor

--- rlang/tests/listener_test.py
+++ rlang/tests/listener_test.py
@@ -1,12 +1,13 @@
 from antlr4 import *
 
+from rlang.src.lmdp.grounding.states.StateGroundingClass import StateFactor
 from rlang.src.language.RLangLexer import RLangLexer
 from rlang.src.language.RLangParser import RLangParser
 from rlang.src.language.RLangListener import RLangListener
 
 
-def listener_from_file(rlang_fname):
-    rlang = FileStream(rlang_fname)
+def listener_from_input(rlang_str):
+    rlang = InputStream(rlang_str)
     lexer = RLangLexer(rlang)
     stream = CommonTokenStream(lexer)
     parser = RLangParser(stream)
@@ -18,4 +19,10 @@ def listener_from_file(rlang_fname):
 
 
 def test_Factor():
-    pass
+    listener = listener_from_input("Factor position := S[0, 1]")
+    position = StateFactor([0, 1], "position")
+    assert listener.new_vars['position'] == position
+
+
+if __name__ == "__main__":
+    test_Factor()
