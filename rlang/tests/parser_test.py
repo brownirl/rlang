848e442ce4cdde8445bfb0dcdd94a9f7089d6c4f
SkittlePox <bspiegel11@gmail.com>
2021-07-27 10:35:10 -0400

    Added simple parser template

--- rlang/tests/parser_test.py
+++ rlang/tests/parser_test.py
@@ -3,11 +3,24 @@ from antlr4 import CommonTokenStream, InputStream, Token
 from rlang.src.language.RLangLexer import RLangLexer
 from rlang.src.language.RLangParser import RLangParser
 
-from .lexer_test import tokenize_from_string
+
+def parse_from_string(input_string):
+    input_stream = InputStream(input_string)
+    lexer = RLangLexer(input_stream)
+    # lexer.addErrorListener(MyErrorListener)
+    stream = CommonTokenStream(lexer)
+    parser = RLangParser(stream)
+    tree = parser.program()
+    return tree
 
 # All tests must begin with 'test_'
 
 
 def test_something():
-    tokens = tokenize_from_string("Predicate f := True")
-    assert len(tokens) == 6
+    tree = parse_from_string("Predicate f := True")
+    print(dir(tree))
+    print(tree.toStringTree())
+
+
+if __name__ == "__main__":
+    test_something()
