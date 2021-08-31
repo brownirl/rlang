82a4ad0ce5a48c8df463f9d044126f5964d7a03b
SkittlePox <bspiegel11@gmail.com>
2021-08-31 14:16:18 -0400

    Added boilerplate for State class

--- rlang/src/grounding/utils/state_space.py
+++ rlang/src/grounding/utils/state_space.py
@@ -1,2 +1,10 @@
 # TODO: This class should contain information about the state space
 # TODO: as well as an actual state representation which will be used by the groundings
+
+import numpy as np
+
+
+class State(np.ndarray):
+    pass
+
+# TODO: This class should abstract away batched states and everything
