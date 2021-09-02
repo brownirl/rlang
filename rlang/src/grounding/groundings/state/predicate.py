1b43389cc80e734fb55ab3b6a314e47205029da8
jennjwang <jennifer123wang@gmail.com>
2021-09-02 11:41:48 -0400

    added predicate tests

--- rlang/src/grounding/groundings/state/predicate.py
+++ rlang/src/grounding/groundings/state/predicate.py
@@ -4,11 +4,12 @@
     author: Benjamin Spiegel (bspiegel@cs.brown.edu), Jennifer Wang
     date: August 2021
 """
-
+from __future__ import annotations
 from typing import Callable
+
+from grounding.groundings import state
 from rlang.src.grounding.groundings.grounding_function import GroundingFunction
 from rlang.src.grounding.utils.domain import Domain
-
 
 class Predicate(GroundingFunction):
     def __init__(self, function: Callable, name: str = None):
@@ -16,10 +17,49 @@ class Predicate(GroundingFunction):
         super().__init__(domain=Domain.STATE, codomain=Domain.BOOLEAN, name=name)
 
     def __call__(self, *args, **kwargs):
-        return self._function(kwargs['state'])     # TODO: it's unclear whether we need to support keywords
+        return self._function(*args, **kwargs)     # TODO: it's unclear whether we need to support keywords
 
 # TODO: Implement __and__, __or__, and __not__ composition
 # TODO: Implement __repr__, __eq__, __lt__, etc.
+
+    def __and__(self, other):
+        if isinstance(other, Predicate):
+            return Predicate(function=lambda *args, **kwargs: self(*args, **kwargs) and other(*args, **kwargs))
+        if isinstance(other, bool):
+            return Predicate(function=lambda *args, **kwargs: self(*args, **kwargs) and bool)
+        #TODO: raise error
+        
+    
+    def __or__(self, other):
+        pass
+
+
+
+
+    
+    ### CONSTANTS #####
+BOOL_TRUE = Predicate(function=lambda *args, **kwargs: True)
+BOOL_FALSE = Predicate(function=lambda *args, **kwargs: False)
+
+if __name__ == "__main__":
+    from rlang.src.grounding import State
+    x = Predicate(function=lambda state: state == State(3))
+    s1 = State(3)
+    print(x(s1))
+    test1 = Predicate(lambda *args, **kwargs: 1 != 1)
+    print(test1())
+    # false_and_false = BOOL_FALSE & (BOOL_FALSE)
+    # true_and_false = BOOL_TRUE & (BOOL_FALSE)
+    # false_and_true = BOOL_FALSE & (BOOL_TRUE)
+    # true_and_true = BOOL_TRUE & (BOOL_TRUE)
+    # print((true_and_true & False)())
+    # print(false_and_false())
+    # print(true_and_false())
+    # print(false_and_true())
+    # print(true_and_true())
+
+class Goal(Predicate):
+    pass    # A Goal is simply a Predicate. We may want to add a constructor which accepts a Predicate
 
 
 class Goal(Predicate):
--- rlang/tests/grounding_tests/predicate_test.py
+++ rlang/tests/grounding_tests/predicate_test.py
@@ -1,12 +1,58 @@
 import unittest
-from rlang.src.grounding import Predicate, State
+from rlang.src.grounding import Predicate, State, BOOL_FALSE, BOOL_TRUE
 
 
 class PredicateTest(unittest.TestCase):
     def test_instantiation(self):
         x = Predicate(function=lambda state: state == State(3))
         s1 = State(3)
+        self.assertTrue(x(s1))
+    
+    def test_true(self):
+        test1 = Predicate(lambda state: 1 == 1, [])
+        self.assertTrue(test1())
+        test2 = Predicate(lambda state: 1 + 1 > 1, [])
+        self.assertTrue(test2())
+        self.assertTrue(BOOL_TRUE())
 
+    def test_false(self):
+        test1 = Predicate(lambda **args: 1 != 1, [])
+        test2 = Predicate(lambda **args: 1 + 1 < 1, [])
+        self.assertFalse(test1())
+        self.assertFalse(test2())
+        self.assertFalse(BOOL_FALSE())
+    
+    def test_and(self):
+        false_and_false = BOOL_FALSE & (BOOL_FALSE)
+        true_and_false = BOOL_TRUE & (BOOL_FALSE)
+        false_and_true = BOOL_FALSE & (BOOL_TRUE)
+        true_and_true = BOOL_TRUE & (BOOL_TRUE)
+        self.assertFalse(true_and_true & (False)())
+        self.assertFalse(false_and_false())
+        self.assertFalse(true_and_false())
+        self.assertFalse(false_and_true())
+        self.assertTrue(true_and_true())
+
+    def test_or(self):
+        false_or_false = BOOL_FALSE | (BOOL_FALSE)
+        true_or_false = BOOL_TRUE | (BOOL_FALSE)
+        false_or_true = BOOL_FALSE | (BOOL_TRUE)
+        true_or_true = BOOL_TRUE | (BOOL_TRUE)
+        self.assertTrue(true_or_false | (1 != 1)())
+        self.assertFalse(false_or_false())
+        self.assertTrue(true_or_false())
+        self.assertTrue(false_or_true())
+        self.assertTrue(true_or_true())
+    
+    def test_not(self):
+        self.assertTrue((not BOOL_FALSE)())
+        self.assertFalse((not BOOL_TRUE)())
+        false1 = Predicate(lambda state: 1 != 1, [])
+        self.assertTrue((not false1)())   
+        true1 = Predicate(lambda state: 1 + 1 > 1, [])
+        self.assertFalse((not true1)())
+        false_or_true = BOOL_FALSE.or_(BOOL_TRUE)
+        self.assertFalse((not false_or_true)())
 
 if __name__ == '__main__':
     unittest.main()
