3fea00fbd9a42ddd50d123cdf7ef7bfea21f7b1e
SkittlePox <bspiegel11@gmail.com>
2021-09-06 13:37:35 -0400

    Removed commented code from GroundingFunction

--- rlang/src/grounding/groundings/grounding_function.py
+++ rlang/src/grounding/groundings/grounding_function.py
@@ -18,27 +18,3 @@ class GroundingFunction(Grounding):
         super().__init__(name)
         self._domain = domain
         self._codomain = codomain
-
-        # self.specification = []  # list of (boolean, expression)
-        #  specification
-
-    # def __call__(self, *args):
-    #     r = []
-    #     for spec in self.specification:
-    #         bs, fs = spec[0](*args), spec[1](*args)
-    #         r.append((bs, fs))
-    #     return r
-    #
-    # def add_specification(self, boolean_cond, function):
-    #     if (
-    #             boolean_cond.domain <= self.domain and self.codomain ==
-    #                   function.codomain and function.domain <= self.domain):
-    #         self.specification.append((boolean_cond, function))
-    #     elif self.domain == boolean_cond.domain:
-    #         raise "Domains are not compatible: " + str(self.domain) + "~="
-    #               + str(boolean_cond.domain)
-    #     else:
-    #         raise "Codomains are not compatible: " + str(self.codomain)
-    #               + "~=" + str(
-    #             boolean_cond.codomain) + " or " + str(self.codomain)
-    #               + "~=" + str(function.codomain)
