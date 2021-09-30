fbd429102170bf9474363269dac0c1e57675ec7d
jennjwang <jennifer123wang@gmail.com>
2021-09-30 15:24:43 -0400

    added feature equality test

--- rlang/tests/grounding_tests/feature_test.py
+++ rlang/tests/grounding_tests/feature_test.py
@@ -16,7 +16,16 @@ class FeatureTest(unittest.TestCase):
         self.assertEqual(test2(state1), 5)
 
     def test_equality(self):
-        pass
+        state1 = State(np.array([2, 3]))
+        pos = Factor([0, 1], "position")
+        x = Feature(pos[0], 1)
+        y = Factor(1, "y")
+        self.assertEqual(x(state=state1), 2)
+
+        test1 = Feature((lambda s: x(state=s) + 3), 1)
+        test2 = Feature((lambda s: x(state=s) + y(state=s)), 1)
+
+        self.assertEqual(test1(state1), test2(state1))
 
 if __name__ == '__main__':
     unittest.main()
