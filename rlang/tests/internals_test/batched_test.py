0e6e84f2257537eb90cc53dd1773fedbe4e09228
jennjwang <jennifer123wang@gmail.com>
2021-10-05 12:21:59 -0400

    added batched_test

--- rlang/tests/internals_test/batched_test.py
+++ rlang/tests/internals_test/batched_test.py
@@ -1,7 +1,63 @@
 import unittest
+from grounding.internals import BatchedPrimitive
+import numpy as np
 
-class ActionSpaceTest(unittest.TestCase):
-    pass
+class BatchedPrimitiveTest(unittest.TestCase):
+
+    def test_new(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        bp = BatchedPrimitive(arr1)
+        self.assertEqual(bp.primitive_size, 3)
+        arr2 = np.ndarray((4, 5))
+        bp2 = BatchedPrimitive(arr2)
+        self.assertEqual(bp2.primitive_size, 5)
+
+    
+    def test_getitem(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        bp = BatchedPrimitive(arr1)
+        self.assertEqual(bp[0], [[1, 4]])
+        self.assertEqual(bp[-1], [[3, 6]])
+        self.assertEqual(bp[1:2].shape, (2, 1))
+        self.assertTrue((bp[:] == arr1).all())
+        self.assertTrue((bp[1:2] == np.array([[2],[5]])).all())
+
+        arr2 = np.ndarray((4, 5))
+        bp2 = BatchedPrimitive(arr2)
+        self.assertEqual(bp2[0].shape, (1, 4))
+        self.assertEqual(bp2[:2].shape, (4, 2))
+        self.assertEqual(bp2[::-1].shape, bp2.shape)
+
+    def test_eq(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        bp = BatchedPrimitive(arr1)
+        self.assertFalse((BatchedPrimitive([[1, 0], [0, 0]]) == BatchedPrimitive(0)).all())
+        self.assertTrue(((bp == arr1) == [[True],[True]]).all())
+        self.assertEqual((bp == arr1).shape, (2, 1))
+        arr2 = np.array([[1, 2, 3], [4, 6, 6]])
+        self.assertTrue(((bp == arr2) == [[True],[False]]).all())
+    
+    def test_unbatched_eq(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        arr2 = np.array([[1, 2, 3], [4, 6, 6]])
+        bp = BatchedPrimitive(arr1)
+        bp1 = BatchedPrimitive(arr2)
+        self.assertTrue(bp.unbatched_eq(bp))
+        self.assertFalse(bp.unbatched_eq(bp1))
+
+    def test_ne(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        arr2 = np.array([[1, 2, 3], [4, 6, 6]])
+        bp = BatchedPrimitive(arr1)
+        bp1 = BatchedPrimitive(arr2)
+        self.assertTrue((BatchedPrimitive([[1, 0], [0, 0]]) != BatchedPrimitive(0)).all())
+        self.assertTrue(((bp != arr1) == [[False],[False]]).all())
+        self.assertEqual((bp != arr1).shape, (2, 1))
+        arr2 = np.array([[1, 2, 3], [4, 6, 6]])
+        self.assertTrue(((bp != arr2) == [[False],[True]]).all())
+        self.assertTrue(((bp != bp1) == [[False],[True]]).all())
+
+        
 
 if __name__ == '__main__':
     unittest.main()
