641e705167614e2fb8d679ebdae39a5b1f2c4839
jennjwang <jennifer123wang@gmail.com>
2021-10-19 15:16:53 -0400

    added more tests/ reworked test directory

--- rlang/tests/listener_test.py
+++ rlang/tests/listener_test.py
@@ -1,196 +1,144 @@
 from __future__ import annotations
+import unittest
 import numpy as np
 import rlang
 from rlang.src.grounding import *
 
+class listenerTests(unittest.TestCase):
 
-def test_Factor():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    state = State(np.array([4, 5, 6, 7, 8]))
+    def test_Factor(self):
+        metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
+        state = State(np.array([4, 5, 6, 7, 8]))
 
-    position_parsed = rlang.parse("Factor position := S[0]", metadata)['position']
-    position = Factor(0, "position")
-    assert position(state=state) == position_parsed(state=state)
-    # assert position_parsed(state=state) == BatchedPrimitive(4)
+        position_parsed = rlang.parse("Factor position := S[0]", metadata)['position']
+        position = Factor(0, "position")
+        assert position(state=state) == position_parsed(state=state)
+        # assert position_parsed(state=state) == BatchedPrimitive(4)
 
-    position_parsed = rlang.parse("Factor position := S[0:3]\nFactor p2 := position[0]", metadata)['position']
-    position = Factor([0, 1, 2], "position")
-    assert position(state=state) == position_parsed(state=state)
+        position_parsed = rlang.parse("Factor position := S[0:3]\nFactor p2 := position[0]", metadata)['position']
+        position = Factor([0, 1, 2], "position")
+        assert position(state=state) == position_parsed(state=state)
 
-    position_parsed = rlang.parse("Factor position := S[0:3]", metadata)['position']
-    assert position(state=state) == position_parsed(state=state)
+        position_parsed = rlang.parse("Factor position := S[0:3]", metadata)['position']
+        assert position(state=state) == position_parsed(state=state)
 
-    position_parsed = rlang.parse("Factor position := S[0, 3, 1]", metadata)['position']
-    position = Factor([0, 3, 1], "position")
-    assert position(state=state) == position_parsed(state=state)
+        position_parsed = rlang.parse("Factor position := S[0, 3, 1]", metadata)['position']
+        position = Factor([0, 3, 1], "position")
+        assert position(state=state) == position_parsed(state=state)
 
-    state2 = State(np.array([[0, 1], [2, 3]]))
-    position_parsed = rlang.parse("Factor position := S[0]", metadata)['position']
-    print(position_parsed(state=state2))
+        state2 = State(np.array([[0, 1], [2, 3]]))
+        position_parsed = rlang.parse("Factor position := S[0]", metadata)['position']
+        position = Factor([0], "position")
+        assert (position_parsed(state=state2) == position(state=state2)).all()
 
-
-def test_Feature():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    state = State(np.array([4, 5, 6, 7, 8]))
-
-    x_parsed = rlang.parse("Feature x := S[0, 1]", metadata)['x']
-    x = Feature.from_Factor(Factor([0, 1]), "x")
-    assert x(state=state) == x_parsed(state=state)
-
-    x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]", metadata)['x']
-    position = Factor([0, 1], "position")
-    x = Feature.from_Factor(position[0], "x")
-    assert x(state=state) == x_parsed(state=state)
-
-    x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position * 2", metadata)['x']
-    position = Factor([0, 1], "position")
-    x = Feature.from_Factor(position * 2, "x")
-    assert x(state=state) == x_parsed(state=state)
-    x = Feature.from_Factor(2 * position, "x")
-    assert x(state=state) == x_parsed(state=state)
-
-    x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0] + 4 * 2 + position[1]", metadata)['x']
-    position = Factor([0, 1], "position")
-    x = Feature(position[0] + 4 * 2 + position[1], "x")
-    assert x(state=state) == x_parsed(state=state)
-
-    x_parsed = rlang.parse("Factor position := S[0:3]\nFeature x := position[0] + 4 * 2 + 3 / position[1]", metadata)['x']
-    position = Factor(list(range(5))[slice(0, 3, 1)], "position")
-    x = Feature(position[0] + 4 * 2 + 3 / position[1], "x")
-    assert x(state=state) == x_parsed(state=state)
-
-    x_parsed = rlang.parse("Feature x := 1 * 2 + 4 * (1 + 2)", metadata)['x']
-    assert x_parsed(state=state) == 14
-
-    x_parsed = rlang.parse("Feature x := [[[2, 1]]] * 2 * S[0]", metadata)['x']
-    print(x_parsed(state=state))
-
-    # x_parsed = rlang.parse("Feature x := [[1, 0], [0, 1, 2]] * 2 * S[0]", metadata)['x']
-    # print(x_parsed(state=state))
-
-    state2 =  State(np.array([[0, 1, 2, 3], [4, 5, 6, 7]]))
-    # x_parsed = rlang.parse("Factor position := S[0:3]\nFeature x := position[0] + 4 * 2 + 3 / position[1]", metadata)[
-    #     'x']
-    x_parsed = rlang.parse("Factor position := S[0:3]\nFeature x := (2 * position + 3) / position[1]", metadata)[
-        'x']
-    print(x_parsed(state=state2))
+        # QUESTION: is more complex indexing allowed? Like Factor test3 := S[5:-1], Factor test4 := S[:]
+        # position_parsed = rlang.parse("Factor position := S[:-1]", metadata)['position']
+        # print(position_parsed(state=state2))
 
 
-def test_Predicate():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    state = State(np.array([4, 5, 6, 7, 8]))
+    def test_Feature(self):
+        metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
+        state = State(np.array([4, 5, 6, 7, 8]))
 
-    hi_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]\nPredicate hi := x == 1 and True or False", metadata)['hi']
-    position = Factor([0, 1], "position")
-    x = Feature(position[0])
-    hi = Predicate(x == 1 & True | False)
-    assert hi(state=state) == hi_parsed(state=state)
+        x_parsed = rlang.parse("Feature x := S[0, 1]", metadata)['x']
+        x = Feature.from_Factor(Factor([0, 1]), "x")
+        assert x(state=state) == x_parsed(state=state)
 
-    hi_parsed = rlang.parse("Predicate hi := True or False and True == True and 14 == 14", metadata)['hi']
-    print(hi_parsed(state=state))
-    assert hi_parsed(state=state) == True
+        x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]", metadata)['x']
+        position = Factor([0, 1], "position")
+        x = Feature.from_Factor(position[0], "x")
+        assert x(state=state) == x_parsed(state=state)
 
-    hi_parsed = rlang.parse("Predicate hi := True or False and False or True", metadata)['hi']
-    print(hi_parsed(state=state))
+        x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position * 2", metadata)['x']
+        position = Factor([0, 1], "position")
+        x = Feature.from_Factor(position * 2, "x")
+        assert x(state=state) == x_parsed(state=state)
+        x = Feature.from_Factor(2 * position, "x")
+        assert x(state=state) == x_parsed(state=state)
 
-    x_parsed = rlang.parse("Predicate x := [0] in [[2], [1], [2, 3]]", metadata)['x']
-    print(x_parsed(state=state))
+        x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0] + 4 * 2 + position[1]", metadata)['x']
+        position = Factor([0, 1], "position")
+        x = Feature(position[0] + 4 * 2 + position[1], "x")
+        assert x(state=state) == x_parsed(state=state)
 
-    x_parsed = rlang.parse("Predicate x := [0, 1] in [[0, 1], [1, 1], [2, 3]]", metadata)['x']
-    print(x_parsed(state=state))
+        x_parsed = rlang.parse("Factor position := S[0:3]\nFeature x := position[0] + 4 * 2 + 3 / position[1]", metadata)['x']
+        position = Factor(list(range(5))[slice(0, 3, 1)], "position")
+        x = Feature(position[0] + 4 * 2 + 3 / position[1], "x")
+        assert x(state=state) == x_parsed(state=state)
 
-    print("Down here")
+        x_parsed = rlang.parse("Feature x := 1 * 2 + 4 * (1 + 2)", metadata)['x']
+        assert x_parsed(state=state) == 14
 
-    state2 = State(np.array([[0, 1], [5, 6]]))
-    x_parsed = rlang.parse("Factor h := S[0, 1]\nPredicate x := h in [[0, 1], [2, 3]]", metadata)['x']
-    print(x_parsed(state=state2))
+        x_parsed = rlang.parse("Feature x := [[[2, 1]]] * 2 * S[0]", metadata)['x']
+        position = Factor([0], "position")
+        #QUESTION: Why does this line error?
+        # x = Feature([[[2, 1]]] * 2 * position, "x")
+        # print(x(state=state))
+        # print(x_parsed(state=state))
 
-    state2 = State(np.array([[0, 1], [5, 6]]))
-    x_parsed = rlang.parse("Predicate x := S[:2] == [0,1]", metadata)['x']
-    print(x_parsed(state=state2))
+        x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0] + position[1]", metadata)['x']
+        assert (x_parsed(state=state)) == 9
 
-    # TODO: Need more tests for Predicate
+        x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]\nFeature y := position[1]/x", metadata)['y']
+        assert (x_parsed(state=state)) == 5/4
+    
+    def test_Predicate(self):
+        metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
+        state = State(np.array([4, 5, 6, 7, 8]))
 
+        hi_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]\nPredicate hi := x == 1 and True or False", metadata)['hi']
+        position = Factor([0, 1], "position")
+        x = Feature(position[0])
+        hi = Predicate(x == 1 & True | False)
+        assert hi(state=state) == hi_parsed(state=state)
 
-def test_Action():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
+        hi_parsed = rlang.parse("Predicate hi := True or False and True == True and 14 == 14", metadata)['hi']
+        assert hi_parsed(state=state) == True
 
-    up_parsed = rlang.parse("Action up := -1.3", metadata)['up']
-    up = ActionReference(-1.3, "up")
-    assert up() == up_parsed()
+        hi_parsed = rlang.parse("Predicate hi := True or False and False or True", metadata)['hi']
+        assert hi_parsed(state=state) == True
 
-    up_parsed = rlang.parse("Action up := [0, 1.0, -4.2]", metadata)['up']
-    up = ActionReference([0, 1.0, -4.2], "up")
-    # assert up() == up_parsed()
+        x_parsed = rlang.parse("Predicate x := [0] in [[2], [1], [2, 3]]", metadata)['x']
+        assert x_parsed(state=state) == [[False]]
 
-    # TODO: Need more tests for Action
+        x_parsed = rlang.parse("Predicate x := [0, 1] in [[0, 1], [1, 1], [2, 3]]", metadata)['x']
+        assert x_parsed(state=state) == [[True]]
 
+        state2 = State(np.array([[0, 1], [5, 6]]))
+        x_parsed = rlang.parse("Factor h := S[0, 1]\nPredicate x := h in [[0, 1], [2, 3]]", metadata)['x']
+        # print(x_parsed(state=state2))
+        #QUESTION: why is this true because isn't [1, 6] not in the array
+        assert x_parsed(state=state2) == [[True]]
 
-def test_Policy():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    s = State([4, 1, 2, 3, 4])
+        state2 = State(np.array([[0, 1], [5, 6]]))
+        x_parsed = rlang.parse("Predicate x := S[0] == [[0], [5]]", metadata)['x']
+        assert ((x_parsed(state=state2)) == [[True], [True]]).all()
 
-    knowledge = rlang.parse_file("tests_resources/listener_tests/policy.rlang", metadata)
-    test1 = knowledge['test1']
-    test2 = knowledge['test2']
+        state2 = State(np.array([[0, 1], [5, 6]]))
+        x_parsed = rlang.parse("Predicate x := S[0] == [[0], [1]]", metadata)['x']
+        assert ((x_parsed(state=state2)) == [[True], [False]]).all()
 
-    print(test1(state=s))
+        #QUESTION: just to confirm, we cannot compare between instances of PrimitiveGroundings
+        # x_parsed = rlang.parse("Factor position := S[0, 1]\nFeature x := position[0]\nFeature y := position[0]\nPredicate test3 := x < y", metadata)['x']
+        # print(x_parsed(state=state2))
 
-    # TODO: Need more tests
+        #QUESTION: how to compare S and S'
 
-
-def test_Option():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    s = State([3, 1, 2, 3, 4])
-
-    knowledge = rlang.parse_file("tests_resources/listener_tests/option.rlang", metadata)
-    build_bridge = knowledge['build_bridge']
-    do = knowledge['do_something']
-
-    print(build_bridge.can_execute(state=s))
-    print(build_bridge(state=s))
-    print(do(state=s))
-
-    # TODO: Need more tests
-
-
-def test_MarkovFeature():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    s = State([3, 1, 2, 3, 4])
-    s2 = State([3, 2, 2, 3, 4])
-
-    # This syntax is cool. It allows you to cast a function of state to a function of next_state
-    knowledge = rlang.parse_file("tests_resources/listener_tests/markov_feature.rlang", metadata)
-    f1 = knowledge['f1']
-    f2 = knowledge['f2']
-    f3 = knowledge['f3']
-    f4 = knowledge['f4']
-    f5 = knowledge['f5']
-
-    print(f1(state=s))
-    print(f2(next_state=s))
-    print(f3(state=s))
-    print(f4(next_state=s))
-    print(f5(state=s, next_state=s2))
+        #QUESTION: can you create a predicate with action
+        # up_parsed = rlang.parse("Action up := -1.3\nPredicate x := up == -1.3", metadata)['x']
+        # print(up_parsed(state=state2))
 
 
-def test_Effect():
-    metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
-    s = State([1, 1, 2, 3, 4])
-    s2 = State([3, 2, 2, 3, 4])
-    action = Action(1)
+    def test_Action(self):
+        metadata = MDPMetadata.from_state_action(np.zeros(5), np.zeros(5))
 
-    knowledge = rlang.parse_file("tests_resources/listener_tests/effect.rlang", metadata)
-    r = knowledge.reward_function
-    print(r(state=s, action=action))
-    t = knowledge.transition_function
-    print(t(state=s, action=action))
+        up_parsed = rlang.parse("Action up := -1.3", metadata)['up']
+        up = ActionReference(-1.3, "up")
+        assert up() == up_parsed()
 
-    predictions = knowledge.predictions
-    print(predictions)
-    print(predictions['f1'](state=s))
-    print(predictions['f2'](state=s))
+        up_parsed = rlang.parse("Action up := [0, 1.0, -4.2]", metadata)['up']
+        up = ActionReference([0, 1.0, -4.2], "up")
+        assert (up() == up_parsed()).all()
 
-
-if __name__ == "__main__":
-    test_Effect()
+if __name__ == '__main__':
+    unittest.main()
