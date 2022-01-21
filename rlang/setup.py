c014e489a026c2370df5fa2dc80955c1294d4b16
SkittlePox <bspiegel11@gmail.com>
2022-01-21 14:18:58 -0500

    anonymized setup.py

--- rlang/setup.py
+++ rlang/setup.py
@@ -5,16 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rlang",
-    version="0.0.1",
-    author="Benjamin Spiegel",
-    author_email="bspiegel@cs.brown.edu",
+    version="1.0.0",
+    author="Anonymous",
+    author_email="",
     description="For specifying partial information about MDPs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rafarodsa/lmdp",
-    # project_urls={
-    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    # },
+    url="",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
