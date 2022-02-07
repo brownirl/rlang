6ddbe1fb38f5a1746c1486c041d85953550d3567
SkittlePox <bspiegel11@gmail.com>
2022-02-07 14:29:28 -0500

    To invoke GH action

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -62,6 +62,7 @@ autodoc_typehints_format = 'short'
 autodoc_inherit_docstrings = True
 
 # autodoc_default_flags = ['members']
+# autosummary_generate = True
 
 
 # -- Options for HTML output -------------------------------------------------
