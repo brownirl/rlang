fc3f470c82711d38f26c3ddcdf9b1ad1e78fce7b
Benjamin Spiegel <bspiegel@cs.brown.edu>
2023-10-26 11:17:42 -0400

    Massaged Factor getitem

--- rlang/rlang/grounding/groundings.py
+++ rlang/rlang/grounding/groundings.py
@@ -526,15 +526,17 @@ class Factor(GroundingFunction):
             raise RLangGroundingError(f"Cannot index factor with given object: {type(item).__name__}")
 
     def __getitem__(self, item):
-        # TODO: Arjan: Reject the item if it's a slice that has negative values in it (i.e. referencing the end of the State array)
+        # If item is a slice, we convert it to a list or tuple, otherwise we just pass it through to get_factor_from_indexer
         if isinstance(item, slice):
+            # Error checking if the slice has negative values
             if item.start < 0 or item.end < 0 or (item.step and item.step < 0):
-                raise RLangGroundingError(f"Cannot use slice object with negative parameter, {item.start}, {item.end}")
-            else:
-                if item.step:
-                    return self.get_factor_from_indexer([i for i in range(item.start, item.end, item.step)])
-                else:
-                    return self.get_factor_from_indexer((item.start, item.end))
+                raise RLangGroundingError(f"Cannot slice factor with negative parameter, {item}")
+
+            if item.step: # slice case with step, we need to return a list of factors
+                return self.get_factor_from_indexer(list(range(item.start, item.end, item.step)))
+            else: # slice case without step, we return a start and end index
+                return self.get_factor_from_indexer((item.start, item.end))
+        
         return self.get_factor_from_indexer(item)
 
     def __hash__(self):
