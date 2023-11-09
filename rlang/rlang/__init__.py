12180c4a679e16014d4651d67a63eacf877e221f
Benjamin Spiegel <bspiegel@cs.brown.edu>
2023-11-09 11:34:33 -0500

    Changed package imports for sake of speedy development

--- rlang/rlang/__init__.py
+++ rlang/rlang/__init__.py
@@ -2,4 +2,4 @@ from .core import parse, parse_file
 from .grounding.groundings import *
 from .grounding.utils.primitives import *
 from .grounding.StateResolverClass import *
-from .agents import *
+# from .agents import *
